2b49bd7683da268498d012de16cf8abbe87b3551
Guilherme Fusuma <fuskenji@gmail.com>
2025-06-12 16:50:52 -0300

    feito tela de area do eleitor

--- area_eleitor.py
+++ area_eleitor.py
@@ -1,6 +1,6 @@
 import flet as ft
 
-def area_eleitor():
+def area_eleitor(nome: str, cpf: str):
     SIZE_IMG = 350
     PROFILEPIC_POS = 150
     PROFILEPIC_SIZE = 120
@@ -21,69 +21,102 @@ def area_eleitor():
 
         return conteudo
 
-    janela = ft.Stack(
-        controls=[
-            ft.Stack( # parte de cima com a imagem
-                controls=[
-                    ft.Image(
-                        src='./imgs/img_area_eleitor.png',
-                        fit=ft.ImageFit.COVER,
-                        width=float('inf')
-                    ),
-                    ft.Container(
-                        bgcolor="#A0000000",
-                        content=ft.Text('Área do eleitor', size=60, weight=ft.FontWeight.BOLD),
-                        expand=True,
-                        alignment=ft.Alignment(-0.6, 0)
-                    )
-                ],
-                height=SIZE_IMG,
-                width=float('inf')
-            ),
-            ft.Container( # parte de baixo
-                margin=ft.Margin(0, SIZE_IMG, 0, 0),
-                bgcolor="#1A3A53"
-            ),
-
-            ft.Container( # janela com informações do usuário
-                height=float('inf'),
-                width=500,
-                bgcolor="#274E6C",
-                border_radius=ft.BorderRadius(40, 0, 40, 0),
-                shadow=ft.BoxShadow(0, 2, ft.Colors.BLACK, (-1, 1)),
-                content=ft.Stack(
+    janela = ft.Container(
+        bgcolor="#1A3A53",
+        expand=True,
+        content=ft.Stack(
+            expand=True,
+            alignment=ft.Alignment(1, -1),
+            controls=[
+                ft.Stack( # parte de cima com a imagem
                     controls=[
-                        ft.Container(
-                            bgcolor="#3471A1",
-                            height=PROFILEPIC_POS
+                        ft.Image(
+                            src='./imgs/img_area_eleitor.png',
+                            fit=ft.ImageFit.COVER,
+                            width=float('inf'),
                         ),
                         ft.Container(
+                            bgcolor="#A0000000"
+                        )
+                    ],
+                    height=SIZE_IMG,
+                    width=float('inf')
+                ),
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.Container( # parte da esquerda
+                            expand=6,
                             content=ft.Column(
                                 controls=[
                                     ft.Container(
-                                        content=ft.Image(src='./imgs/tiririca.png', width=PROFILEPIC_SIZE),
-                                        border_radius=99999999999999999999999999999999999999999999999999999999999999
+                                        content=ft.Text('Área do eleitor', size=60, weight=ft.FontWeight.BOLD),
+                                        height=SIZE_IMG,
+                                        width=float('inf'),
+                                        alignment=ft.Alignment(0, 0)
                                     ),
                                     ft.Column(
+                                        alignment=ft.MainAxisAlignment.CENTER,
+                                        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                        spacing=40,
+                                        expand=True,
+                                        width=float('inf'),
                                         controls=[
-                                            info('Nome do(a) eleitor(a)', 'Neymar Silva Bueno'),
-                                            info('CPF', '6969696969')
-                                        ],
-                                        width=float('inf')
+                                            ft.ElevatedButton(
+                                                text='Reistrar voto',
+                                                color=ft.Colors.WHITE,
+                                                bgcolor="#3471A1",
+                                                width=220,
+                                                scale=1.5
+                                            ),
+                                            ft.ElevatedButton(
+                                                text='Visualizar candidatos',
+                                                color=ft.Colors.WHITE,
+                                                bgcolor="#3471A1",
+                                                width=220,
+                                                scale=1.5
+                                            )
+                                        ]
+                                    )
+                                ]
+                            )
+                        ),
+                        ft.Container( # janela com informações do usuário
+                            expand=3,
+                            bgcolor="#274E6C",
+                            border_radius=ft.BorderRadius(40, 0, 40, 0),
+                            shadow=ft.BoxShadow(0, 2, ft.Colors.BLACK, (-1, 1)),
+                            content=ft.Stack(
+                                controls=[
+                                    ft.Container(
+                                        bgcolor="#3471A1",
+                                        height=PROFILEPIC_POS
+                                    ),
+                                    ft.Container(
+                                        content=ft.Column(
+                                            controls=[
+                                                ft.Image(src='./imgs/tiririca.png', width=PROFILEPIC_SIZE, border_radius=100),
+                                                ft.Column(
+                                                    controls=[
+                                                        info('Nome do(a) eleitor(a)', nome),
+                                                        info('CPF', cpf)
+                                                    ],
+                                                    width=float('inf')
+                                                )
+                                            ],
+                                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                            spacing=60
+                                        ),
+                                        padding=ft.Padding(20, PROFILEPIC_POS - PROFILEPIC_SIZE/2, 20, 0)
                                     )
                                 ],
-                                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                                spacing=60
-                            ),
-                            padding=ft.Padding(20, PROFILEPIC_POS - PROFILEPIC_SIZE/2, 20, 0)
+                                alignment=ft.Alignment(0, -1)
+                            )
                         )
-                    ],
-                    alignment=ft.Alignment(0, -1)
-                )
-            ) 
-        ],
-        expand=True,
-        alignment=ft.Alignment(1, -1)
+                    ]
+                ) 
+            ],
+        )
     )
 
     return janela
@@ -92,8 +125,7 @@ def main(page: ft.Page):
     page.title = "Área do eleitor"
     page.padding = 0
 
-    a_eleitor = area_eleitor()
+    page.add(area_eleitor('Neymar da Silva Bueno', 6969696969))
 
-    page.add(a_eleitor)
-
-ft.app(main)
+if __name__ == "__main__":
+    ft.app(main)
